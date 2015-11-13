3aebab1c0d5b58d60ad50f497f5888cd6cc62a3e
Pagan Gazzard <pjgazzard@googlemail.com>
2015-11-13 14:02:40 +0000

    Apparently someone was confused about the meaning of "first"

--- automation/jenkins-build.sh
+++ automation/jenkins-build.sh
@@ -4,12 +4,11 @@ set -e
 VERSION=$(git rev-parse --short HEAD)
 ESCAPED_BRANCH_NAME=$(echo $sourceBranch | sed 's/[^a-z0-9A-Z_.-]/-/g')
 
-docker build --tag resin/${JOB_NAME}:${VERSION} .
-IMAGE_NAME=resin/${JOB_NAME}:${VERSION} $(dirname $0)/test.sh
-
 # Try pulling the old build first for caching purposes.
 docker pull resin/${JOB_NAME}:${ESCAPED_BRANCH_NAME} || docker pull resin/${JOB_NAME}:master || true
 
+docker build --tag resin/${JOB_NAME}:${VERSION} .
+IMAGE_NAME=resin/${JOB_NAME}:${VERSION} $(dirname $0)/test.sh
 docker tag -f resin/${JOB_NAME}:${VERSION} resin/${JOB_NAME}:${ESCAPED_BRANCH_NAME}
 
 # Push the images
