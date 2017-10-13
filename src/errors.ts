20a7972a47700b65096d0531a86c4ba9f347bccb
Pagan Gazzard <page@resin.io>
2017-10-13 08:21:10 -0700

    Simplify errors.ts
    Change-type: patch

--- src/errors.ts
+++ src/errors.ts
@@ -1,16 +1,12 @@
-import * as _ from 'lodash';
 import * as _Raven from 'raven';
 
-export const Raven = (() => {
-	_.once(() =>
-		_Raven.config(process.env.SENTRY_DSN || false, {
-			captureUnhandledRejections: true,
-			release: process.env.npm_package_version,
-			environment: process.env.NODE_ENV,
-		}).install()
-	)();
-	return _Raven;
-})();
+export const Raven = _Raven;
+
+Raven.config(process.env.SENTRY_DSN || false, {
+	captureUnhandledRejections: true,
+	release: process.env.npm_package_version,
+	environment: process.env.NODE_ENV,
+}).install();
 
 export const captureException = (err: Error, message?: string, options?: _Raven.CaptureOptions): string => {
 	console.error(message, (err.message ? err.message : err), err.stack);
