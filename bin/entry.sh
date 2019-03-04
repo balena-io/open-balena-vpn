9250616f5c5008735e8b5aecee2a3b4ce37e6fd0
Will Boyce <me@willboyce.com>
2019-03-04 20:51:37 +0000

    core: Add sourcemap support when running javascript
    Change-type: patch
    Signed-off-by: Will Boyce <will@balena.io>

--- bin/entry.sh
+++ bin/entry.sh
@@ -5,16 +5,19 @@ WORK_DIR="$(readlink -e "$(dirname "$0")"/..)"
 PATH="${WORK_DIR}/node_modules/.bin:${PATH}"
 cd "${WORK_DIR}"
 
+# shellcheck disable=1090
 test -f "${WORK_DIR}/config/env" && source "${WORK_DIR}/config/env"
 
 service="$(basename "$0")"
 service="${service/balena-/}"
 command="$(command -v node)"
+args=("-r" "source-map-support/register")
 entrypoint="build/src/${service}/app.js"
 if [ "${NODE_ENV}" = "development" ]; then
 	tsnode="$(command -v ts-node || true)"
 	if [ -x "${tsnode}" ]; then
 		command="${tsnode}"
+		args=()
 		entrypoint="src/${service}/app.ts"
 	fi
 fi
@@ -24,4 +27,4 @@ if [ ! -x "${command}" ] || [ ! -f "${entrypoint}" ]; then
 	exit 1
 fi
 
-exec "${command}" "${entrypoint}" "$@"
+exec "${command}" "${args[@]}" "${entrypoint}" "$@"
