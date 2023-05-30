eefc7c26057ebf69fa531508ec196aef583ab7cc
ab77 <anton@balena.io>
2023-05-30 16:22:42 -0700

    update copyright
    change-type: patch

--- openvpn/scripts/client-connect-disconnect.sh
+++ openvpn/scripts/client-connect-disconnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2014 Balena Ltd.
+# Copyright (C) 2023 Balena Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
