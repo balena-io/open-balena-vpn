e3420cc0727ae351db8b73165bacd40ac3ee8dc5
Pagan Gazzard <page@balena.io>
2025-03-01 12:00:09 +0000

    Propagate the trace id to the api on pinejs requests
    Change-type: minor

--- src/api.ts
+++ src/api.ts
@@ -22,7 +22,7 @@ import express from 'express';
 import memoize from 'memoizee';
 import morgan from 'morgan';
 
-import { getLogger } from './utils/index.js';
+import { getLogger, getPassthrough } from './utils/index.js';
 import {
 	BALENA_API_INTERNAL_HOST,
 	DELAY_ON_AUTH_FAIL,
@@ -51,7 +51,7 @@ const checkDeviceAuth = memoize(
 	async (username: string, password: string) => {
 		const { statusCode } = await pooledRequest.get({
 			url: `${BALENA_API_INTERNAL_HOST}/services/vpn/auth/${username}`,
-			headers: { Authorization: `Bearer ${password}` },
+			...getPassthrough(`Bearer ${password}`),
 		});
 		if ([200, 401, 403].includes(statusCode)) {
 			return statusCode;
