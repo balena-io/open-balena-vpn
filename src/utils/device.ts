9c48429e9ca2d09cee8e3a1a8b1fee0998555642
Thodoris Greasidis <thgreasi@gmail.com>
2025-09-30 23:55:37 +0300

    getDeviceVpnHost: Make the raw is_connected_to_vpn filter more explicit using the alias
    Change-type: patch
    See: https://balena.fibery.io/Security/Information_Security_and_Reliability_Incident/Elevated-Device-VPN-Tunnel-Errors-150

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -113,7 +113,7 @@ export const getDeviceVpnHost = async (
 							$alias: 'd',
 							$expr: {
 								d: { uuid },
-								$: 'is_connected_to_vpn',
+								$raw: 'd/is_connected_to_vpn',
 							},
 						},
 					},
